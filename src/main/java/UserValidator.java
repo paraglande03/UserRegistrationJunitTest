39fe88ad1a2d21e3ea3a56570654b12f84f45aa2
paraglande03 <paraglande03@gmail.com>
2021-02-21 16:33:53 +0530

    Added Test Cases for email

--- src/main/java/UserValidator.java
+++ src/main/java/UserValidator.java
@@ -5,10 +5,19 @@ public class UserValidator {
 
 
         private final String NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
+    private final String EMAIL_PATTERN = "^[a-z]+[A-Z0-9.+_-]+[@][a-zA-Z0-9]+[.]co(m|.in)$";
 
         public boolean validateFirstName(String firstName) {
             Pattern pattern = Pattern.compile(NAME_PATTERN);
             return pattern.matcher(firstName).matches();
+        }
+        public boolean validateLastName(String lastName) {
+            Pattern pattern = Pattern.compile(NAME_PATTERN);
+            return pattern.matcher(lastName).matches();
+        }
+        public boolean validateEmail(String email) {
+            Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+            return pattern.matcher(email).matches();
         }
 
 }
--- src/test/java/TestUserValidator.java
+++ src/test/java/TestUserValidator.java
@@ -35,5 +35,60 @@ public class TestUserValidator {
             Assert.assertEquals(false, result);
         }
 
+    @Test
+    public void givenLastName_WhenLastNameStartsWithCapitalLetter_ShouldReturn_True() {
+        UserValidator userValidator = new UserValidator();
+        boolean result = userValidator.validateLastName("Lande");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenLastName_WhenLastNameNotStartsWithCapitalLetter_ShouldReturn_False() {
+        UserValidator userValidator = new UserValidator();
+        boolean result = userValidator.validateLastName("lande");
+        Assert.assertEquals(false, result);
+    }
+
+    @Test
+    public void givenLastName_WhenLastNameHaveMinimumThreeLetter_ShouldReturn_True() {
+        UserValidator userValidator = new UserValidator();
+        boolean result = userValidator.validateLastName("Lan");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenLastName_WhenLastNameNotHaveMinimumThreeLetter_ShouldReturn_False() {
+        UserValidator userValidator = new UserValidator();
+        boolean result = userValidator.validateLastName("la");
+        Assert.assertEquals(false, result);
+    }
+        @Test
+        public void givenEmail_WhenEmailStartsWithSmallLetter_ShouldReturn_True() {
+            UserValidator userValidator = new UserValidator();
+            boolean result = userValidator.validateEmail("paraglande03@gmail.com");
+            Assert.assertEquals(true, result);
+        }
+
+        @Test
+        public void givenEmail_WhenEmailNotStartsWithSmallLetter_ShouldReturn_False() {
+            UserValidator userValidator = new UserValidator();
+            boolean result = userValidator.validateEmail("Parag@gmail.com");
+            Assert.assertEquals(false, result);
+        }
+
+        @Test
+        public void givenEmail_WhenEmailEndsWithComOrIn_ShouldReturn_True() {
+            UserValidator userValidator = new UserValidator();
+            boolean result = userValidator.validateEmail("abc111@yahoo.com");
+            Assert.assertEquals(true, result);
+        }
+
+        @Test
+        public void givenEmail_WhenEmailNotEndsWithComOrIn_ShouldReturn_False() {
+            UserValidator userValidator = new UserValidator();
+            boolean result = userValidator.validateEmail("Abc@gmail.kjnk");
+            Assert.assertEquals(false, result);
+        }
+
 
 }
